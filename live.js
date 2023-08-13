d9cbd40bfc961a078339ab222a7d52b618191f6d
Mert Salik <53333294+codermert@users.noreply.github.com>
2023-08-13 16:47:13 +0300

    Update live.js

--- live.js
+++ live.js
@@ -1,33 +1,28 @@
 const fs = require('fs');
-const https = require('https');
+const axios = require('axios');
 const cheerio = require('cheerio');
+const UserAgent = require('user-agents');
 
 const baseUrl = 'https://kamuilan.sbb.gov.tr/';
 
-const options = {
-  hostname: 'kamuilan.sbb.gov.tr',
-  path: '/',
-  method: 'GET'
+const userAgent = new UserAgent();
+const headers = {
+  'User-Agent': userAgent.toString(),
 };
 
-const req = https.request(options, response => {
-  let data = '';
+axios.get(baseUrl, { headers })
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-  response.on('data', chunk => {
-    data += chunk;
-  });
-
-  response.on('end', () => {
     const ilanlar = [];
-
-    const $ = cheerio.load(data);
 
     $('#nav2 > li').each((index, element) => {
       const timeElement = $(element).find('.cbp_tmtime');
       const labelElement = $(element).find('.cbp_tmlabel');
 
-      const date = timeElement.find('h4').text() + " " + timeElement.find('h3').text();
-      
+      const date = timeElement.find('h4').text() + ' ' + timeElement.find('h3').text();
+
       labelElement.find('a').each((aIndex, aElement) => {
         const a = $(aElement);
         const logoPath = a.find('.limg2').attr('src');
@@ -39,25 +34,24 @@ const req = https.request(options, response => {
           tarih: date,
           logo: logoUrl,
           baslik: title,
-          aciklama: description
+          aciklama: description,
         });
       });
     });
 
     const jsonData = JSON.stringify(ilanlar, null, 2);
+    console.log(jsonData);
+    console.log(JSON.stringify(userAgent.data, null, 2));
 
-    fs.writeFile('ilanlar.json', jsonData, 'utf8', err => {
+
+    fs.writeFile('ilanlar.json', jsonData, 'utf8', (err) => {
       if (err) {
         console.error('Dosya kaydedilirken bir hata oluştu:', err);
       } else {
-        console.log('Veriler "ilanlar.json" dosyasına kaydedildi.');
+        // console.log('Veriler "ilanlar.json" dosyasına kaydedildi.');
       }
     });
+  })
+  .catch(error => {
+    console.error('Hata:', error);
   });
-});
-
-req.on('error', error => {
-  console.error('Hata:', error);
-});
-
-req.end();
